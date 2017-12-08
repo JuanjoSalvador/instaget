4fafca701badbc542edc4fddeddd2eadf3cb293c
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-12-08 17:04:11 +0100

    uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'instaget',
+  packages = ['instaget'], # this must be the same as the name above
+  version = '0.1',
+  description = 'CLI tool for downloading Instagram pictures',
+  author = 'Juanjo Salvador',
+  author_email = 'juanjosalvador@netc.eu',
+  url = 'https://github.com/juanjosalvador/instaget', # use the URL to the github repo
+  download_url = 'https://github.com/juanjosalvador/instaget/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['instagram', 'downloader'], # arbitrary keywords
+  classifiers = [],
+)
